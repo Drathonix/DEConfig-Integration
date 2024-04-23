999977529d26d5c4690a4894adcbdb65f68f8cb7
Drathonix <52723768+Drathonix@users.noreply.github.com>
2024-04-23 10:30:42 -0400

    Manually added missing tags class

---
+++ src/main/java/com/drathonix/deconfigintegration/Tags.java
@@ -0,0 +1,7 @@
+package com.drathonix.deconfigintegration;
+
+public class Tags {
+
+    public static final String VERSION = "1.0.1";
+    public static final String MODID = "deconfigintegration";
+}
