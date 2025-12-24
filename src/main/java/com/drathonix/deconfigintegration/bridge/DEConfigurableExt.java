4ca2b98a9c8702d3465e44eb80af557d261b9c8b
Drathonix <52723768+Drathonix@users.noreply.github.com>
2025-12-23 22:37:18 -0500

    Webhook test

--- src/main/java/com/drathonix/deconfigintegration/bridge/DEConfigurableExt.java
+++ src/main/java/com/drathonix/deconfigintegration/bridge/DEConfigurableExt.java
@@ -5,8 +5,12 @@ import javax.annotation.Nonnull;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
+/**
+ * Can be implemented by an item class to provide draconic evolution with a configurable tag to read and modify.
+ * @author Jack Andersen
+ * @since 1.0.0
+ */
 public interface DEConfigurableExt {
-
     default @Nonnull NBTTagCompound deci$getConfigurableTag(@Nonnull ItemStack stack) {
         return stack.stackTagCompound;
     }
