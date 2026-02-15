019dd81dc807648c4984b77e819bfa90f9fd64f4
Drathonix <52723768+Drathonix@users.noreply.github.com>
2026-02-15 11:35:30 -0500

    1.0.3

--- src/main/java/com/drathonix/deconfigintegration/mixins/emt/MixinItemElectricBootsTraveller.java
+++ src/main/java/com/drathonix/deconfigintegration/mixins/emt/MixinItemElectricBootsTraveller.java
@@ -3,7 +3,6 @@ package com.drathonix.deconfigintegration.mixins.emt;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.drathonix.deconfigintegration.DECIMixins;
 import com.drathonix.deconfigintegration.DEConfigIntegration;
 import com.drathonix.deconfigintegration.bridge.TranslatableItemConfigField;
 import net.minecraft.item.ItemStack;
@@ -32,7 +31,7 @@ public class MixinItemElectricBootsTraveller implements DEConfigurableExt, IConf
             new EnumerableItemConfigField(References.DOUBLE_ID, slot, "jump", "boots.jumpPercentage")
                 .setMinMaxAndIncromente(0D, 1D, 0.05D)
                 .readFromItem(stack, 0.5D));
-        if(DEConfigIntegration.thaumicBoots) {
+        if(DEConfigIntegration.thaumicboots) {
             fields.add(
                 new TranslatableItemConfigField(References.BOOLEAN_ID, slot, "omni", "boots.omni")
                     .readFromItem(stack, true));
