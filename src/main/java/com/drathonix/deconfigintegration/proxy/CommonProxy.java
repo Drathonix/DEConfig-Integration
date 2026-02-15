4d53c77cb7c4b9bb954e948ccc5ac64b5af241df
Drathonix <52723768+Drathonix@users.noreply.github.com>
2026-02-15 11:47:08 -0500

    actually spotless apply

--- src/main/java/com/drathonix/deconfigintegration/proxy/CommonProxy.java
+++ src/main/java/com/drathonix/deconfigintegration/proxy/CommonProxy.java
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 public class CommonProxy {
 
     // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed) a
+    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
 
     }
